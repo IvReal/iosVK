edbc5ef681ef4b104b4db3d35f11a88f32343eaf
IvReal <ivreal@yandex.ru>
2019-03-22 15:04:00 +0300

    Image spring animation

--- Lesson1.1/Lesson1.1/Controls/LikeControl.swift
+++ Lesson1.1/Lesson1.1/Controls/LikeControl.swift
@@ -110,7 +110,7 @@ class LikeControl: UIControl {
     @objc private func updateLikeCount(_ sender: UIButton) {
         doneLike = !doneLike
         countLike = countLike + 1 * (doneLike ? 1 : -1)
-        animate()
+        animate(doneLike)
         if let handler = changeLikeHandler {
             handler(countLike, doneLike)
         }
@@ -121,14 +121,14 @@ class LikeControl: UIControl {
         stackView.frame = bounds
     }
     
-    private func animate() {
+    private func animate(_ doneLike: Bool) {
         UIView.transition(with: self.buttonHeart,
                           duration: 1.5,
                           options: .transitionCrossDissolve,
                           animations: nil)
         UIView.transition(with: self.buttonCount,
                           duration: 1.5,
-                          options: .transitionFlipFromLeft,
+                          options: doneLike ? .transitionFlipFromRight : .transitionFlipFromLeft,
                           animations: nil)
     }
 }
